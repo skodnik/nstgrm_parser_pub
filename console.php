bc5fe03b0e372704aa563250d8c921a71c333b84
vlsv <g.skodnik@gmail.com>
2019-05-28 20:59:47 +0300

    README correction. console.php -rm. Add bin/console

--- console.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/App/autoload.php';
-
-$console = new App\Controllers\Console(getopt('us::a:r::c::i:b:d:'));
-$console();
